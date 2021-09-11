62fb9bb81c466e4282efa407515e372518e51574
Solomon King <50495497+1solomonking@users.noreply.github.com>
2021-09-11 09:50:48 +0100

    Add files via upload

---
+++ app/src/main/java/android/example/gksreminder/AnnSeclectCategory.java
@@ -0,0 +1,13 @@
+package android.example.gksreminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnnSeclectCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ann_seclect_category);
+    }
+}
